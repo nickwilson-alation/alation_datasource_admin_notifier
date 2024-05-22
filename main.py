1649282bb8b31159bf0092bc25e9fe0ba092d284
Nick Wilson <nicholas.wilson@alation.com>
2024-05-22 16:17:52 -0600

    lots of work

--- main.py
+++ main.py
@@ -4,50 +4,62 @@ import requests
 import smtplib
 from email.message import EmailMessage
 
-def fetch_all_datasource_ids():
-    return [1, 2, 3]
+def fetch_all_datasources(base_url, headers):
+    url = f'https://{base_url}/integration/v2/datasource/'
+    response = requests.get(url, headers=headers)
 
-def get_last_mde_job_status_by_ds_id(ds_id):
-    if ds_id == 1:
-        return 'FAILED'
-    if ds_id == 2:
-        return 'SUCCESSFUL'
-    if ds_id == 3:
-        return 'SUCCESSFUL'
+    if response.status_code == 200:
+        data = response.json()
+        return data
 
-def get_ds_owner_ids_by_ds_id(ds_id):
-    return [100, 101]
+def did_last_mde_job_succeed_by_ds_id(ds_id, base_url, headers):
+    url = f'https://{base_url}/api/v1/job/?job_type=0&external_service_otype=data&external_service_aid={ds_id}&skip=0&limit=1&order_by=-ts_started&with_num_errors=true&enable_server_count=true'
+    response = requests.get(url, headers=headers)
+    last_job_status = ''
 
-def get_ds_owner_email_address_by_user_id(user_id):
-    if user_id == 100:
-        return 'nicholas.wilson@alation.com'
-    if user_id == 101:
-        return 'matt.sullivan@alation.com'
+    if response.status_code == 200:
+        data = response.json()
+        if len(data) > 0:
+            last_job_status = data[0]["status"]
+            print(f'Last job status: {last_job_status}')
+        else: # MDE has never ran on the source; skip the rest
+            return True
+              
+    if last_job_status == 'SUCCEEDED':
+        return True
+    else: 
+        return False
 
-def get_ds_owner_email_addresses_by_ds_id(ds_id):
-    ds_owner_ids = get_ds_owner_ids_by_ds_id(ds_id)
+def get_ds_owner_email_address_by_user_id(user_id, base_url, headers):
+    owner_email_address = ''
+    url = f'https://{base_url}/integration/v1/user/{user_id}'
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        owner_email_address = data['email']
+
+    return owner_email_address
+
+def get_email_owner_addresses_by_ds(ds, base_url, headers):
     ds_owner_email_addresses = []
-    for user_id in ds_owner_ids:   
-        ds_owner_email_address = get_ds_owner_email_address_by_user_id(user_id)
+    for user_id in ds['owner_ids']:   
+        ds_owner_email_address = get_ds_owner_email_address_by_user_id(user_id, base_url, headers)
         ds_owner_email_addresses.append(ds_owner_email_address)
     return ds_owner_email_addresses
 
-def send_email(recipients, subject, body):
-    msg = EmailMessage()
-    msg.set_content(body)
-    msg['Subject'] = subject
-    msg['From'] = "your_email@example.com"
-    msg['To'] = ', '.join(recipients)
+def email_admins_by_email_address(ds, ds_owner_email_addresses):
+    ds_title = ds['title']
+    ds_id = ds['id']
+    subject = f'Last Metadata Extraction (MDE) Job Failed for Data Source: "{ds_title}" (DS ID: {ds_id})'
+    body = f'Last Metadata Extraction (MDE) Job Failed for Data Source: "{ds_title}" (DS ID: {ds_id})'
+    send_email_by_addresses_subject_and_body(ds_owner_email_addresses, subject, body)
 
-    with smtplib.SMTP('smtp.example.com', 587) as server:
-        server.starttls()
-        server.login("your_email@example.com", "your_password")
-        server.send_message(msg)
-
-def notify_ds_admins_by_email(ds_id, ds_owner_email_addresses):
-    subject = f'Metadata Extraction (MDE) Job Failed for Data Source ID: {ds_id}'
-    body = f'Metadata Extraction (MDE) Job Failed for Data Source ID: {ds_id}'
-    send_email(ds_owner_email_addresses, subject, body)
+def send_email_by_addresses_subject_and_body(email_addresses, subject, body):
+    print('Skipping email, as function not yet implemented')
+    print(f'Recipients: {email_addresses}')
+    print(f'Subject: {subject}')
+    print(f'Body: {body}')
 
 def main():
     # Create arg parser
@@ -74,21 +86,26 @@ def main():
 
     api_token = args.token
     base_url = args.base_url
+    headers = {
+        'TOKEN': api_token,
+        'accept': 'application/json, text/plain, */*'
+    }
 
     # 1. Get all datasource IDs
-    all_datasource_ids = fetch_all_datasource_ids()
-    for ds_id in all_datasource_ids:
+    all_datasources = fetch_all_datasources(base_url, headers)
+    for ds in all_datasources:
+        ds_id = ds['id']
         print(f'Checking status of MDE for ds_id: {ds_id}')
 
         # 2. Get status of last MDE job
-        last_mde_job_status = get_last_mde_job_status_by_ds_id(ds_id)
+        did_last_mde_job_succeed = did_last_mde_job_succeed_by_ds_id(ds_id, base_url, headers)
         
-        if last_mde_job_status == "FAILED":
+        if did_last_mde_job_succeed == True:
+            print('succeeded!')
+        else:
             # 3. If applicable, email datasource admins
-            ds_owner_email_addresses = get_ds_owner_email_addresses_by_ds_id(ds_id)
-            print(f'Will email the following datasource admins: {", ".join(ds_owner_email_addresses)}')
-            notify_ds_admins_by_email(ds_owner_email_addresses)
-
+            ds_owner_email_addresses = get_email_owner_addresses_by_ds(ds, base_url, headers)
+            email_admins_by_email_address(ds, ds_owner_email_addresses)
 
 if __name__ == "__main__":
     main()
